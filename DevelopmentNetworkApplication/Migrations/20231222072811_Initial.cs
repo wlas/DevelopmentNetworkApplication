9f41aac92dcaa452070860c8d3eedf6e63dc74ce
SandrLite <mr.wlas@mail.ru>
2023-12-22 10:31:26 +0300

    HomeWork5

---
+++ DevelopmentNetworkApplication/Migrations/20231222072811_Initial.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DevelopmentNetworkApplication.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FullName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("userPk", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    messageText = table.Column<string>(type: "text", nullable: true),
+                    messageData = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_sent = table.Column<bool>(type: "boolean", nullable: false),
+                    UserToId = table.Column<int>(type: "integer", nullable: true),
+                    UserFromId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("messagePk", x => x.id);
+                    table.ForeignKey(
+                        name: "messageFromUserFK",
+                        column: x => x.UserFromId,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "messageToUserFK",
+                        column: x => x.UserToId,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_UserFromId",
+                table: "messages",
+                column: "UserFromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_UserToId",
+                table: "messages",
+                column: "UserToId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_FullName",
+                table: "users",
+                column: "FullName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "messages");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ DevelopmentNetworkApplication/UDPServer.cs
@@ -0,0 +1,141 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace DevelopmentNetworkApplication
+{
+    internal class UDPServer
+    {
+        class Server
+        {
+            Dictionary<String, IPEndPoint> clients = new Dictionary<string, IPEndPoint>();
+            UdpClient udpClient;
+
+            void Register(NetMessage message, IPEndPoint fromep)
+            {
+                Console.WriteLine("Message Register, name = " + message.NickNameFrom);
+                clients.Add(message.NickNameFrom, fromep);
+
+
+                using (var ctx = new ChatContext())
+                {
+                    if (ctx.Users.FirstOrDefault(x => x.FullName == message.NickNameFrom) != null) return;
+
+                    ctx.Add(new User { FullName = message.NickNameFrom });
+
+                    ctx.SaveChanges();
+                }
+            }
+
+            void ConfirmMessageReceived(int? id)
+            {
+                Console.WriteLine("Message confirmation id=" + id);
+
+                using (var ctx = new ChatContext())
+                {
+                    var msg = ctx.Messages.FirstOrDefault(x => x.MessageId == id);
+
+                    if (msg != null)
+                    {
+                        msg.IsSent = true;
+                        ctx.SaveChanges();
+                    }
+                }
+            }
+
+            void RelyMessage(NetMessage message)
+            {
+                int? id = null;
+                if (clients.TryGetValue(message.NickNameTo, out IPEndPoint ep))
+                {
+                    using (var ctx = new ChatContext())
+                    {
+                        var fromUser = ctx.Users.First(x => x.FullName == message.NickNameFrom);
+                        var toUser = ctx.Users.First(x => x.FullName == message.NickNameTo);
+                        var msg = new Message() { UserFrom = fromUser, UserTo = toUser, IsSent = false, Text = message.Text };
+                        ctx.Messages.Add(msg);
+
+                        ctx.SaveChanges();
+
+                        id = msg.MessageId;
+                    }
+
+
+                    var forwardMessageJson = new NetMessage()
+                    {
+                        Id = id,
+                        Command = Command.Message,
+                        NickNameTo = message.NickNameTo,
+                        NickNameFrom = message.NickNameFrom,
+                        Text = message.Text
+                    }.SerialazeMessageToJSON();
+
+                    byte[] forwardBytes = Encoding.ASCII.GetBytes(forwardMessageJson);
+
+                    udpClient.Send(forwardBytes, forwardBytes.Length, ep);
+                    Console.WriteLine($"Message Relied, from = {message.NickNameFrom} to = {message.NickNameTo}");
+                }
+                else
+                {
+                    Console.WriteLine("Пользователь не найден.");
+                }
+            }
+
+            void ProcessMessage(NetMessage message, IPEndPoint fromep)
+            {
+                Console.WriteLine($"Получено сообщение от {message.NickNameFrom} для {message.NickNameTo} с командой {message.Command}:");
+                Console.WriteLine(message.Text);
+
+
+                if (message.Command == Command.Register)
+                {
+                    Register(message, new IPEndPoint(fromep.Address, fromep.Port));
+
+                }
+                if (message.Command == Command.Confirmation)
+                {
+                    Console.WriteLine("Confirmation receiver");
+                    ConfirmMessageReceived(message.Id);
+                }
+                if (message.Command == Command.Message)
+                {
+                    RelyMessage(message);
+                }
+            }
+
+
+            public void Work()
+            {
+
+                IPEndPoint remoteEndPoint;
+
+                udpClient = new UdpClient(12345);
+                remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                Console.WriteLine("UDP Клиент ожидает сообщений...");
+
+                while (true)
+                {
+                    byte[] receiveBytes = udpClient.Receive(ref remoteEndPoint);
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine(receivedData);
+
+                    try
+                    {
+                        var message = NetMessage.DeserializeMessgeFromJSON(receivedData);
+
+                        ProcessMessage(message, remoteEndPoint);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Ошибка при обработке сообщения: " + ex.Message);
+                    }
+                }
+
+            }
+        }
+    }
+}
